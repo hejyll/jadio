c6763110b81d689f4e21c59e478abe8680fda5ba
Hejiko Hejio <hejyll@gmail.com>
2024-06-09 07:47:47 +0900

    remove search.py

--- src/jadio/__init__.py
+++ src/jadio/__init__.py
@@ -5,5 +5,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 from ._version import __version__
 from .platforms import *  # NOQA
 from .program import Program  # NOQA
-from .search import *  # NOQA
 from .station import Station  # NOQA

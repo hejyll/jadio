d90c13e2d5078c5a29302822977dd56b668504bc
Hejiko Hejio <hejyll@gmail.com>
2024-06-10 17:00:46 +0900

    update test_onsen.py

--- tests/jadio/services/test_onsen.py
+++ tests/jadio/services/test_onsen.py
@@ -1,29 +1,37 @@
-import os
 import tempfile
+from pathlib import Path
 
+import pytest
 from jadio import Onsen
 from jadio.util import check_dict_deep, get_login_info_from_config
 
+try:
+    LOGIN_INFO = get_login_info_from_config("radiko.jp")
+except FileNotFoundError:
+    LOGIN_INFO = None
+
 
 def test_login_without_user_info():
-    with Onsen() as radio:
-        assert radio._driver.name == "chrome"
+    with Onsen() as service:
+        service: Onsen
+        assert service._driver.name == "chrome"
 
 
+@pytest.mark.skipif(not LOGIN_INFO, reason="config file is not found")
 def test_login_with_user_info():
-    login_info = get_login_info_from_config("radiko.jp")
-    with Onsen(**login_info) as radio:
-        assert len(radio._driver.get_cookies()) > 0
+    login_info = get_login_info_from_config("onsen.ag")
+    with Onsen(**login_info) as service:
+        assert len(service._driver.get_cookies()) > 0
 
 
 def test_login_with_user_info_error():
-    with Onsen(mail="hoge@gmail.com", password="xxx") as radio:
-        assert len(radio._driver.get_cookies()) > 0
+    with Onsen(mail="hoge@gmail.com", password="xxx") as service:
+        assert len(service._driver.get_cookies()) > 0
 
 
-def test_get_information():
-    with Onsen() as radio:
-        information = radio._get_information()
+def test__get_information():
+    with Onsen() as service:
+        information = service._get_information()
         assert check_dict_deep(information, ["state", "programs", "programs", "all"])
         raw = information["state"]["programs"]["programs"]["all"][0]
         need_keys = [
@@ -57,24 +65,14 @@ def test_get_information():
 
 
 def test_get_programs():
-    with Onsen() as radio:
-        programs = radio.get_programs()
+    with Onsen() as service:
+        programs = service.get_programs()
         assert len(programs) > 0
 
 
-def test_get_programs_with_filters():
-    with Onsen() as radio:
-        information = radio._get_information()
-        raw = information["state"]["programs"]["programs"]["all"][0]
-        target_id = raw["directory_name"]
-        programs = radio.get_programs(filters=[target_id])
-        for program in programs:
-            assert program.ascii_name == target_id
-
-
 def test_download():
-    with Onsen() as radio, tempfile.TemporaryDirectory() as tmp_dir:
-        programs = radio.get_programs()
-        filename = os.path.join(tmp_dir, "hoge.mp4")
-        radio.download(programs[0], filename)
-        assert os.path.exists(filename)
+    with Onsen() as service, tempfile.TemporaryDirectory() as tmp_dir:
+        program = service.get_programs()[0]
+        file_path = Path(tmp_dir) / "media.m4a"
+        service.download(program, file_path)
+        assert file_path.exists()
